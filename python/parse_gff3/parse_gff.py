90e65e1a7ee45670629da759a5cf7ec94a3720bd
Juan Felipe Ortiz <juan@biosilico.io>
2025-07-26 17:18:30 +0800

    multiline comments

--- python/parse_gff3/parse_gff.py
+++ python/parse_gff3/parse_gff.py
@@ -15,14 +15,18 @@ def parse_attrs(attrs_line) -> dict:
 
 
 def parse_gff3(in_file: Path, source):
-    # Working with gzipped files is very similar to working with normal text files. Since both functions use a similar function signature, I can just use the appropriate function with the same arguments, and it will just work (Note, this might be different when working with binary files)
+    # Working with gzipped files is very similar to working with normal text files.
+    # Since both functions use a similar function signature, I can just use the appropriate
+    # function with the same arguments, and it will just work
+    # (Note, this might be different when working with binary files)
     open_fx = gopen if in_file.suffix == ".gz" else open
     parsed_features = []
     with open_fx(in_file, "rt") as file_h:
         for line in file_h:
             if not line.startswith("#"):
                 if source in line:
-                    # I can use list destructuring to assign the elements of a list (the result of the `split` method) to individual variables. [Line 33]
+                    # I can use list destructuring to assign the elements of a list
+                    # (the result of the `split` method) to individual variables.
                     (
                         chrom,
                         _source,
@@ -33,19 +37,28 @@ def parse_gff3(in_file: Path, source):
                         strand,
                         _phase,
                         attrs,
-                        # String manipulation is straight-forward. Most string methods can be chained. In that way, I can keep adding methods to my parsing functionality and see the result seach time. This pattern makes string-parsing easy in Python
+                        # String manipulation is straight-forward. Most string methods can be chained.
+                        # In that way, I can keep adding methods to my parsing functionality and see the result seach time.
+                        # This pattern makes string-parsing easy in Python
                     ) = line.strip().split("\t")
                     attrs_parsed = parse_attrs(attrs)
                     annotation = {
                         "region": chrom,
                         "feature_type": feature_type,
-                        # Casting variables to other data types is easy [Lines 38, 39]. In this case, we can assume that the columns four and five will always be integers, however, in non-trivial cases, casting variables to other data types can crash the script or silently assign data to the wrong type.
+                        # Casting variables to other data types is easy. In this case, we can assume that the columns
+                        # four and five will always be integers, however, in non-trivial cases, casting variables to
+                        # other data types can crash the script or silently assign data to the wrong type.
                         "start": int(start),
                         "end": int(end),
                         "strand": strand,
                         "attrs": attrs_parsed,
                     }
-                    # Adding elements to a list is quite easy. However, keep in mind that Python will generate a new list and copy all elements in the list plus the new element every time a list is updated, which will have performance issues with long lists. Ideally, if we know the size of the list in advance, it is ideal to initialize a list with empty items and assign elements to the list rather than appending new element to it. If the size of the list is not known in advance, several techniques exist to ameliorate the performance issues that appending to a list bring.
+                    # Adding elements to a list is quite easy. However, keep in mind that Python will generate a new
+                    # list and copy all elements in the list plus the new element every time a list is updated, which
+                    # will have performance issues with long lists. Ideally, if we know the size of the list in advance,
+                    # it is ideal to initialize a list with empty items and assign elements to the list rather than
+                    # appending new element to it. If the size of the list is not known in advance, several techniques
+                    # exist to ameliorate the performance issues that appending to a list bring.
                     parsed_features.append(annotation)
     return parsed_features
 
