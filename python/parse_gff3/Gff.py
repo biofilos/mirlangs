f01fba863c03a61d51dc25f63fc0a1297ca36540
Juan Felipe Ortiz <juan@biosilico.io>
2025-07-26 15:44:10 +0800

    annotate python code

--- python/parse_gff3/Gff.py
+++ python/parse_gff3/Gff.py
@@ -3,8 +3,12 @@ from pathlib import Path
 from dataclasses import dataclass
 from gzip import open as gzopen
 
+
 class BadGffVersion(Exception):
-    def __init__(self, message="GFF is not version 3, or there is no version information in the header of the file"):
+    def __init__(
+        self,
+        message="GFF is not version 3, or there is no version information in the header of the file",
+    ):
         super().__init__(message)
 
 
@@ -22,13 +26,19 @@ class Gff:
     def __init__(self, gff_file: Path) -> None:
         self.gff_file = gff_file
         self.meta = self.parse_meta()
-        self.feats = [GffFeature(
-            x["region"], x["feature_type"],
-            x["start"], x["end"], x["strand"], x["attrs"]
-        ) for x in parse_gff3(gff_file, "ensembl_havana")]
+        self.feats = [
+            GffFeature(
+                x["region"],
+                x["feature_type"],
+                x["start"],
+                x["end"],
+                x["strand"],
+                x["attrs"],
+            )
+            for x in parse_gff3(gff_file, "ensembl_havana")
+        ]
         self.n_features = len(self.feats)
         self.n_regions = len({x.region for x in self.feats})
-
 
     def parse_meta(self):
         open_fx = gzopen if self.gff_file.suffix == ".gz" else open
@@ -39,7 +49,11 @@ class Gff:
                     key, *line_data = line.lstrip("##").lstrip("#!").split()
                     key = key.replace("-", "_")
                     if key not in meta_d:
-                        meta_d[key] = " ".join(line_data) if line.startswith("#!") or "gff_version" == key else [line_data]
+                        meta_d[key] = (
+                            " ".join(line_data)
+                            if line.startswith("#!") or "gff_version" == key
+                            else [line_data]
+                        )
                     else:
                         meta_d[key].append(line_data)
                 else:
@@ -48,7 +62,7 @@ class Gff:
             fixed_regions = [None] * len(meta_d["sequence_region"])
             for ix, region_data in enumerate(meta_d["sequence_region"]):
                 region, start, end = region_data
-                region_dict = dict(region=region, start=int(start),end=int(end))
+                region_dict = dict(region=region, start=int(start), end=int(end))
                 fixed_regions[ix] = region_dict
 
         meta_d["regions"] = fixed_regions
@@ -58,9 +72,11 @@ class Gff:
         return meta_d
 
     def __str__(self):
-        info = (f"GFF file: {self.gff_file}\n"
-                "Summary\n"
-                f"{self.n_features} features in {self.n_regions} regions")
+        info = (
+            f"GFF file: {self.gff_file}\n"
+            "Summary\n"
+            f"{self.n_features} features in {self.n_regions} regions"
+        )
         return info
 
 
--- python/parse_gff3/parse_gff.py
+++ python/parse_gff3/parse_gff.py
@@ -8,18 +8,21 @@ def parse_attrs(attrs_line) -> dict:
     :param attrs_line: GFF3-formatted annotation attributes
     :return: dictionary of attributes
     """
+    # List and dictionary comprehensions are powerful tools to easily parse collections of data into dictionary and list data structures. **If** they are short and concise, they are quite easy to read. However, for complex processing logic, use a for-loop
     items = [x.split("=") for x in attrs_line.split(";")]
     ann_raw = {x[0]: x[1] for x in items}
     return ann_raw
 
 
 def parse_gff3(in_file: Path, source):
+    # Working with gzipped files is very similar to working with normal text files. Since both functions use a similar function signature, I can just use the appropriate function with the same arguments, and it will just work (Note, this might be different when working with binary files)
     open_fx = gopen if in_file.suffix == ".gz" else open
     parsed_features = []
     with open_fx(in_file, "rt") as file_h:
         for line in file_h:
             if not line.startswith("#"):
                 if source in line:
+                    # I can use list destructuring to assign the elements of a list (the result of the `split` method) to individual variables. [Line 33]
                     (
                         chrom,
                         _source,
@@ -30,16 +33,19 @@ def parse_gff3(in_file: Path, source):
                         strand,
                         _phase,
                         attrs,
+                        # String manipulation is straight-forward. Most string methods can be chained. In that way, I can keep adding methods to my parsing functionality and see the result seach time. This pattern makes string-parsing easy in Python
                     ) = line.strip().split("\t")
                     attrs_parsed = parse_attrs(attrs)
                     annotation = {
                         "region": chrom,
                         "feature_type": feature_type,
+                        # Casting variables to other data types is easy [Lines 38, 39]. In this case, we can assume that the columns four and five will always be integers, however, in non-trivial cases, casting variables to other data types can crash the script or silently assign data to the wrong type.
                         "start": int(start),
                         "end": int(end),
                         "strand": strand,
                         "attrs": attrs_parsed,
                     }
+                    # Adding elements to a list is quite easy. However, keep in mind that Python will generate a new list and copy all elements in the list plus the new element every time a list is updated, which will have performance issues with long lists. Ideally, if we know the size of the list in advance, it is ideal to initialize a list with empty items and assign elements to the list rather than appending new element to it. If the size of the list is not known in advance, several techniques exist to ameliorate the performance issues that appending to a list bring.
                     parsed_features.append(annotation)
     return parsed_features
 
