582ca28c6939db73f5b2ef4ad97fa4b3bec3be5e
Juan Felipe Ortiz <juan@biosilico.io>
2025-08-04 22:47:10 +0800

    Move comments from post to code

--- elixir/parse_gff3/lib/parse_all_gff3.ex
+++ elixir/parse_gff3/lib/parse_all_gff3.ex
@@ -1,3 +1,4 @@
+# Structs. In Elixir, structs are similar to structs in Rust or Go. Basically, we declare a module with named fields. This is usually not absolutely required, but it can give the code more clarity, and it is very useful when matching functions that have to interact with specific structs. In this case, we have 2 named structs `GFFfeature`, and `RegionHeader`  that will be executed by different versions of `collapse_regions` depending on what kind of struct is passed to the function.
 defmodule RegionHeader do
   defstruct [:region, :start, :end]
 end
@@ -15,6 +16,7 @@ defmodule ParseAllGff3 do
     parse_file()
   end
 
+  # Matching functions to the beginning of a string. In order to rexecute a function when a string starts with a specific substring, we use the pattern `"##" <> rest` in the function signature. The `<>` operator concatenate strings, so in this case, the argument passed to the function will try to be reconstructed with the `<>` operator where the match will be successful only if the first part of the string is `"##"`. As a bonus, the variable `rest` can be used in the body of the function.
   def parse("##" <> rest) do
     [ix | body] = String.trim(rest, "\n") |> String.split()
 
@@ -37,6 +39,7 @@ defmodule ParseAllGff3 do
     end
   end
 
+  # List processing. In Elixir, lists can be destructured by using the syntax `[head | tail]`. This will assign the first element of the list to `head`, and the rest of the list to `tail`. Similary, one can add elements efficiently to a list with the pattern `[new_element | old_list]`. The issue with this, is that the new element is added to the beginning of the list, so we have to reverse the list at the end. This pattern is so common, that Elixir has optimized this process, so there is not a big performance penalties for using this approach.
   def parse("#!" <> rest) do
     [ix | body] = String.trim(rest, "\n") |> String.split()
     [first | rest] = body
@@ -76,6 +79,7 @@ defmodule ParseAllGff3 do
     parsed_features =
       Path.expand("../../../data/gff3_parsing/Homo_sapiens.GRCh38.114.gff3.gz", __DIR__)
       |> ParseGff3.stream_file()
+      # Map and anonymous functions. Anonymous functions can be defined in different ways. In this case , the syntax `&parse/1` means "convert the function `parse` that accepts one argument (`/1`) to an anonymous function", and now we can use `parse/1` in our pipeline.
       |> Stream.map(&parse/1)
       |> Enum.reduce(%{regions: %{}, features: [], metadata: %{}}, &collapse_regions/2)
 
--- elixir/parse_gff3/lib/parse_gff3.ex
+++ elixir/parse_gff3/lib/parse_gff3.ex
@@ -4,6 +4,7 @@ defmodule ParseGff3 do
   end
 
   def stream_file(filepath) do
+    # Cases. In Elixir, we can use function guards to define a function several times, matching a condition in the guard. In this case, I chose to use cases. They look similar to Rust's match expression. For functions with just a few lines, I like this better, as it is less verbose.
     case Path.extname(filepath) do
       ".gz" ->
         File.stream!(filepath, [:compressed])
@@ -18,11 +19,15 @@ defmodule ParseGff3 do
   end
 
   def parse_attrs(attrs_line) do
+    # List comprehensions. Elixir can do list comprehensions. In this case, `into: %{}` tells Elixir to create a map, and the little pipeline below, send a tuple of `{key, value}` to the resulting map.
     for attr <- String.split(attrs_line, ";"), into: %{} do
       String.split(attr, "=") |> List.to_tuple()
     end
   end
 
+  # Having the same function name with different argumentns is called function overloading. The way it works is similar to a `switch` statement in other languages. In this case, the arguments are matched to the structure of arguments in the function definitions in the order they are defined. If the match succeeds, that function will execute, and if it does not, the next function will be matched. I like this, because it makes function definitions more consice, as we can have a definition for each case.
+
+  # Function pattern mathing. In Elixir, the `=` sign is not necessarily "equal", but a pattern matching operator. There is a whole discussion about it in several books, and it is outside of the scope of this article. For now, what is relevant is that it vcan be used to declare variables, but also to destructure and structurally match arguments from functions. For example, extract annotation will receive an argument, if that argument is a list with 9 elements, it will destructure the argument and assign the elements to `chrom`, `_source`, etc, and return a map with those values. If the argument cannot be destructured in 9 elements, the next `extract_annotation` will be matched. In this case, the `extract_annotation` just passes the data along.
   def extract_annotation([
         chrom,
         _source,
@@ -47,12 +52,16 @@ defmodule ParseGff3 do
   def extract_annotation(data), do: data
 
   def parse do
+    # Pipelines. If you are familiar with bash pipes, this will feel natural. In a nutshell, a pipeline sends the result of the previous function as the first argument of the next function. This is very convenient, as in a lot of data processing tasks, we take the output of one procedure to further process it with different programs.
     Path.expand("../../../data/gff3_parsing/Homo_sapiens.GRCh38.114.gff3.gz", __DIR__)
+
+    # Streams. Streams in Elixir are lazy, so they don't load the whole file into memory. Instead, they read the file line by line, and send each line to the next function. This is very convenient for large files, as we don't need to load the whole file into memory. Most functions from the `Enum` module have implementations in `Stream`.
     |> stream_file()
     |> Stream.filter(fn x -> !String.starts_with?(x, "#") end)
     |> Stream.filter(fn x -> String.contains?(x, "ensembl_havana") end)
     |> Stream.map(fn x -> String.trim(x, "\n") |> String.split("\t") end)
     |> Stream.map(&extract_annotation/1)
+    # Finishing with `Enum`. Returns values from `Stream` are lazy loaded. This means that they do not produce any persistent values, unless we trigger the computation. In this case, Sending the stream to `Enum.to_list` will force the computation, and return a list with all the values.
     |> Enum.to_list()
   end
 end
