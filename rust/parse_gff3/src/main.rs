a6ac678336e4560b840e5bcccea160acbb92a654
Juan Felipe Ortiz <juan@biosilico.io>
2025-08-13 11:40:16 +0800

    Rust implementation

--- rust/parse_gff3/src/main.rs
+++ rust/parse_gff3/src/main.rs
@@ -1,6 +1,50 @@
 use flate2::read::GzDecoder;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+type Attrs = HashMap<String, String>;
+
+#[derive(Debug)]
+struct Gene {
+    chromosome: String,
+    start: i32,
+    end: i32,
+    strand: String,
+    attrs: Attrs,
+}
+
+fn parse_attrs(attrs_line: String) -> Attrs {
+    let attrs: Attrs = attrs_line
+        .split(";")
+        .map(|x| {
+            let mut split = x.split("=");
+            let key = split.next().unwrap().to_string();
+            let value = split.next().unwrap().to_string();
+            (key, value)
+        })
+        .collect();
+    attrs
+}
+
+impl Gene {
+    fn new(gff_line: String) -> Self {
+        let parts = gff_line.split("\t").collect::<Vec<&str>>();
+        let chromosome = parts[0].to_string();
+        let start = parts[3].parse::<i32>().unwrap();
+        let end = parts[4].parse::<i32>().unwrap();
+        let strand = parts[6].to_string();
+        let attrs_line = parts[8].to_string();
+        let attrs = parse_attrs(attrs_line);
+        Self {
+            chromosome,
+            start,
+            end,
+            strand,
+            attrs,
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Region {
@@ -11,6 +55,7 @@ struct Region {
 struct Annotation {
     gff_version: u16,
     regions: Vec<Region>,
+    genes: Vec<Gene>,
 }
 
 impl Annotation {
@@ -18,11 +63,9 @@ impl Annotation {
         Self {
             gff_version: 0,
             regions: Vec::new(),
+            genes: Vec::new(),
         }
     }
-}
-
-impl Annotation {
     fn set_version(&mut self, gff_line: String) {
         let version_str = gff_line.split(" ").nth(1).unwrap().to_string();
         let version_num = match version_str.parse::<u16>() {
@@ -40,6 +83,9 @@ impl Annotation {
         };
         let name = fields[1].to_string();
         self.regions.push(Region { name, length });
+    }
+    fn add_gene(&mut self, gff_line: String) {
+        self.genes.push(Gene::new(gff_line));
     }
 }
 
@@ -66,6 +112,9 @@ fn main() {
                     line if line.starts_with("##sequence-region") => {
                         annotation.add_region(line);
                     }
+                    line if line.contains("\tensembl_havana\tgene\t") => {
+                        annotation.add_gene(line);
+                    }
                     _ => {
                         // Ignore other lines
                     }
@@ -74,5 +123,5 @@ fn main() {
             Err(e) => panic!("Error reading line: {e}"),
         }
     }
-    println!("{annotation:?}");
+    println!("{:?}", annotation);
 }
